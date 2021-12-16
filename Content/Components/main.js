1f03441f580d8973429311c5a5526c2068191f1a
nathanile-gambo <nathanielgambo2020@gmail.com>
2021-12-16 15:37:13 +0100

    Structuring, development and responsive design completed. Implementing text contents.

---
+++ Content/Components/main.js
@@ -0,0 +1,21 @@
+// "Use Strict"
+
+// // data storage
+// const rootElement = document.documentElement;
+
+// // navbarOnScroll gets changed
+
+// let navigationBar = document.querySelector('nav');
+
+// document.addEventListener('scroll', navbarScrollFunction);
+
+// function navbarScrollFunction() {
+//     let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+
+//     if((rootElement.scrollTop / scrollTotal) > 0.0) {
+//         // change navigation bar background color
+//         navigationBar.classList.add('bg-white');
+//     } else {
+//         navigationBar.classList.remove('bg-white');
+//     }
+// }
